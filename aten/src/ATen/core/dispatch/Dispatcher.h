350bc862096a32d8af41bd0b8f0feae1de515e4c
ilia-cher <iliacher@fb.com>
2020-04-30 12:58:05 -0700

    [wip] RecordFunction in Dispatcher
    Summary:
    Lifting RecordFunction up into the dispatcher code
    
    ghstack-source-id: 7214d088471cffc8a67074b59d2009592f823c38
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37587

--- aten/src/ATen/core/dispatch/Dispatcher.h
+++ aten/src/ATen/core/dispatch/Dispatcher.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <ATen/FunctionSequenceNumber.h>
 #include <ATen/core/dispatch/OperatorEntry.h>
 #include <ATen/core/dispatch/RegistrationHandleRAII.h>
+#include <ATen/record_function.h>
 #include <c10/util/Exception.h>
 #include <c10/util/LeftRight.h>
 #include <mutex>
 #include <list>
+
+#include <ATen/core/boxing/impl/boxing.h>
 
 namespace c10 {
 
@@ -283,11 +287,97 @@ namespace detail {
 template<class... Args> inline void unused_arg_(const Args&...) {}
 }
 
+template <typename T>
+inline c10::IValue get_ivalue_copy(const T& v) {
+  return c10::IValue();
+}
+
+#define IVALUE_COPY_FOR_TYPE(T) \
+    template <> \
+    inline c10::IValue get_ivalue_copy<T>(const T& v) { \
+      return v; \
+    }
+
+IVALUE_COPY_FOR_TYPE(at::Tensor);
+IVALUE_COPY_FOR_TYPE(at::Scalar);
+IVALUE_COPY_FOR_TYPE(double);
+IVALUE_COPY_FOR_TYPE(int64_t);
+IVALUE_COPY_FOR_TYPE(int32_t);
+IVALUE_COPY_FOR_TYPE(bool);
+
+// currently not working with incomplete type
+template <typename T, std::enable_if_t<c10::impl::not_ok_to_box<T>::value>* = nullptr>
+inline bool push_ivalue_copy(std::vector<c10::IValue>& stack, const T& v) {
+  // don't do anything with stack
+  return false;
+}
+template <typename T, std::enable_if_t<!c10::impl::not_ok_to_box<T>::value>* = nullptr>
+inline bool push_ivalue_copy(std::vector<c10::IValue>& stack, const T& v) {
+  torch::jit::push(stack, v);
+  return true;
+}
+
+// attempt to workaround some(?) incomplete types
+
+// complete and possible to instantiate
+template<typename T, typename F = void>
+struct is_instanceable {
+  static const bool value = false;
+};
+
+template<typename T>
+struct is_instanceable<T, std::void_t<decltype(sizeof(T()))>> {
+  static const bool value = true;
+};
+
+template <typename T, std::enable_if_t<!is_instanceable<T>::value>* = nullptr>
+inline bool push_ivalue_copy_2(std::vector<c10::IValue>& stack, const T& v) {
+  // don't do anything with stack
+  return false;
+}
+
+template <typename T, std::enable_if_t<is_instanceable<T>::value>* = nullptr>
+inline bool push_ivalue_copy_2(std::vector<c10::IValue>& stack, const T& v) {
+  bool to_push = (std::is_constructible<IValue, T>::value &&
+      std::is_convertible<T, IValue>::value) || std::is_same<TensorOptions, T>::value;
+  if (to_push) {
+    torch::jit::push(stack, v);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+template<typename T, typename F = void>
+struct is_instanceable_2 {
+  //static const bool value = false;
+};
+
+template<typename T>
+struct is_instanceable_2<T, std::void_t<decltype(sizeof(T()))>> {
+  static const bool value = true;
+};
+
 template<class Return, class... Args>
 inline Return Dispatcher::callUnboxedWithDispatchKey(const OperatorHandle& op, DispatchKey dispatchKey, Args... args) const {
   detail::unused_arg_(args...);  // workaround for a false-positive warning about unused parameters in gcc 5
   const auto& dispatchTable = op.operatorIterator_->op.dispatch_table();
   const KernelFunction& kernel = dispatch_(dispatchTable, dispatchKey);
+
+  // RECORD_FUNCTION macro will attempt to box the arguments only if we have active observers enabled
+  RECORD_FUNCTION(op.schema().name(), std::vector<c10::IValue>{get_ivalue_copy(args)...}, at::FunctionSequenceNumber::peek());
+
+  // test
+  std::vector<c10::IValue> stack;
+  auto v = std::vector<bool>{push_ivalue_copy_2(stack, args)...};
+
+  // check if we can box some useful types here
+  bool t1 = is_instanceable_2<at::Tensor>::value;
+  bool t2 = is_instanceable_2<at::Scalar>::value;
+  // sanity check
+  bool t3 = is_instanceable_2<double>::value;
+
   return kernel.template callUnboxed<Return, Args...>(op, std::forward<Args>(args)...);
 }
 
@@ -308,6 +398,8 @@ inline Return Dispatcher::callUnboxedRedispatch(const OperatorHandle& op, Dispat
     DispatchKeySet(DispatchKeySet::FULL_AFTER, currentDispatchKey),
     args...);
   const KernelFunction& kernel = dispatch_(dispatchTable, dispatchKey);
+
+  // Note: not attempting to use RECORD_FUNCTION to avoid double logging
   return kernel.template callUnboxed<Return, Args...>(op, std::forward<Args>(args)...);
 }
 
@@ -316,6 +408,10 @@ inline void Dispatcher::callBoxed(const OperatorHandle& op, Stack* stack) const 
   const auto& dispatchTable = op.operatorIterator_->op.dispatch_table();
   auto dispatchKey = dispatchTable.dispatchKeyExtractor().getDispatchKeyBoxed(backendsWithoutFallthrough_, stack);
   const KernelFunction& kernel = dispatch_(dispatchTable, dispatchKey);
+
+  // using already existing stack to record function execution in observers
+  RECORD_FUNCTION(op.schema().name(), *stack, at::FunctionSequenceNumber::peek());
+
   kernel.callBoxed(op, stack);
 }
 
